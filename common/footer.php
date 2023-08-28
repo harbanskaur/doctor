e74cdabd897f24c232571dae3252f444a0300049
harbanskaur <142726867+harbanskaur@users.noreply.github.com>
2023-08-28 13:08:32 +0530

    Delete common directory

--- common/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer goes here  -->
-		<div class="main3">
-				<p>Location : India,Punjab<br>
-				Contact Us : +91 10321-11006</p>
-		</div>
-		<!-- footer ends here  -->
-	</div>
-	<!--main container ends here-->
